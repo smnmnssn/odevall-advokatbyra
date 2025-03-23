06ac7eb640d1ddc8a0e7cc0fa68c49f897266974
Simon <simonmansson@live.se>
2025-03-23 14:44:29 +0100

    cmt

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,13 @@
-import { Bodoni_Moda } from "next/font/google";
+// import { Bodoni_Moda } from "next/font/google";
 import "./globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
-const bodoni = Bodoni_Moda({
-  subsets: ["latin"],
-  weight: ["400", "700"],
-  // Du kan även ange exempelvis "900" om du behöver den vikten
-  display: "swap",
-});
+// const bodoni = Bodoni_Moda({
+//   subsets: ["latin"],
+//   weight: ["400", "700"],
+//   // Du kan även ange exempelvis "900" om du behöver den vikten
+//   display: "swap",
+// });
 
 export default function RootLayout({
   children,
